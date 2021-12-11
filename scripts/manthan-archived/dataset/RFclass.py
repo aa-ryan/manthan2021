18de1aebafb0895dbde55f749d03ad54e756dabe
aa-ryan <aryansinha6363@gmail.com>
2021-12-11 06:13:04 +0530

    deleted old

--- scripts/manthan-archived/dataset/RFclass.py
+++
@@ -1,6 +0,0 @@
-spark = SparkSession.builder.appName("Indian Crime Dataset")\
-.config("spark.some.config.option", "some-value")\
-.config("spark.driver.memory", "4g")\
-.config("spark.driver.cores", "1")\
-.getOrCreate()
-
